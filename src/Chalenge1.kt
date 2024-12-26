d6add5b569a3e40a2e893a1e58f0614427053459
Cavin <cavinlarry001@gmail.com>
2024-12-26 15:40:51 +0300

    Initial commit

--- src/Chalenge1.kt
+++ src/Chalenge1.kt
@@ -1,7 +1,8 @@
 // Even odd number checker
 
 fun main(){
-    val num = readln().toInt()
+    print("Enter a number: ")
+    var num = readln().toInt()
     if (num % 2 == 0){
         println("$num is even")
     } else {
