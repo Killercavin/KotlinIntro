e5e875fbe7f30411439d241f8ecf8a269d8d36ea
Cavin <cavinlarry001@gmail.com>
2026-07-18 07:02:23 +0300

    Add initial C source files for chapter 02 examples

---
+++ clab/chapter02/declarations.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int lower, upper, step;
+    char c, line[1000];
+    
+    // string declaration
+    char my_string[] = "";
+
+    int string_length = strlen(my_string);
+
+    printf("%d\n", string_length);
+
+    return 0;
+}
---
+++ clab/chapter02/enums.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    enum Status {ONLINE, OFFLINE, LOS};
+
+    printf("%d\n", LOS);
+}
