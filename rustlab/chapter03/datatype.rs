b4e910e31de2dbf8c5c9e99c2031cc0a4915d695
Cavin <cavinlarry001@gmail.com>
2026-08-01 17:40:20 +0300

    Init Rust forge...

---
+++ rustlab/chapter03/datatype.rs
@@ -0,0 +1,4 @@
+fn main() {
+    let mut x: u32 = 5;
+    x = -5;
+}
---
+++ rustlab/chapter03/variable.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let mut x: i32 = 5;
+
+    println!("The value of x before: {x}");
+    x = 'r' as i32; // char
+
+    println!("The value of x after: {x}");
+}
