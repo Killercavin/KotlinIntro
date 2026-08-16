196f3e44a12f922d2a668d7520791134caacebfa
Cavin <114779273+devcavin@users.noreply.github.com>
2026-08-16 20:43:02 +0300

    update [javalab]

--- javalab/src/javalab/basics/Strings.java
+++ javalab/src/javalab/basics/Strings.java
@@ -2,7 +2,7 @@ package javalab.basics;
 
 public class Strings {
     public static void main(String[] args) {
-        String myString = "This is a test string";
+        String myString = "Some text";
         System.out.println(myString);
     }
 }
